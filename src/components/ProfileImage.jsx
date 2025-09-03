7875454b7fb0d4c18c1b6ecc8e35fdb807571d3f
Katie Cook <contact@katiecook.co.uk>
2025-09-03 21:13:47 +0100

    Changed CSS

--- src/components/ProfileImage.jsx
+++ src/components/ProfileImage.jsx
@@ -6,7 +6,7 @@ export default function ProfileImage({ imageUrl, alt }) {
     <div
       className="profile-image"
       style={{
-        background: `url('${imageUrl}') lightgray -82.229px -105.26px / 201.081% 251.351% no-repeat`
+        background: `url('${imageUrl}') lightgray -210.346px -269.259px / 201.081% 251.351% no-repeat`
       }}
       role="img"
       aria-label={alt}
--- src/components/TriangleBackground.jsx
+++ src/components/TriangleBackground.jsx
@@ -1,71 +1,23 @@
-// Define and export a functional React component named "TriangleBackground"
+import PurpleTriangle from "../images/purple-gradient-triangle-background.svg";
+import GreyTriangle from "../images/grey-gradient-triangle-background.svg";
+
+// Functional React component to display two triangle background images
 export default function TriangleBackground() {
   return (
     <>
-
-      {/* First triangle SVG with purple gradient background */}
-      <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        width="600" 
-        height="237" 
-        viewBox="0 0 600 237" 
-        fill="none" 
+      {/* Purple gradient triangle background */}
+      <img
+        src={PurpleTriangle}
         className="triangle-background"
-      >
+        alt="purple-gradient-triangle-background"
+      />
 
-        {/* Triangle path filled with a vertical purple gradient */}
-        <path 
-          d="M0 0H600V237L0 117V0Z" 
-          fill="url(#paint0_linear_56_4)" 
-        />
-        <defs>
-
-          {/* Define vertical linear gradient for the first triangle */}
-          <linearGradient 
-            id="paint0_linear_56_4" 
-            x1="300" 
-            y1="0" 
-            x2="300" 
-            y2="237" 
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#7244D1" />
-            <stop offset="1" stop-color="#7244D1" stop-opacity="0.70" />
-          </linearGradient>
-        </defs>
-      </svg>
-
-      {/* Second triangle SVG with gray/white gradient background */}
-      <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        width="600" 
-        height="583" 
-        viewBox="0 0 600 583" 
-        fill="none" 
+      {/* Gray/white gradient triangle background */}
+      <img
+        src={GreyTriangle}
         className="triangle-background-1"
-      >
-
-        {/* Triangle path filled with a vertical white/gray gradient */}
-        <path 
-          d="M0 0H600V344.5L1.12057e-05 582.5L0 0Z" 
-          fill="url(#paint0_linear_56_43)" 
-        />
-        <defs>
-          
-          {/* Define vertical linear gradient for the second triangle */}
-          <linearGradient 
-            id="paint0_linear_56_43" 
-            x1="300" 
-            y1="541.5" 
-            x2="300" 
-            y2="0" 
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#EDEDED" stop-opacity="0"/>
-            <stop offset="0.756532" stop-color="#E8E8E8"/>
-          </linearGradient>
-        </defs>
-      </svg>
+        alt="grey-gradient-triangle-background"
+      />
     </>
   );
 }
